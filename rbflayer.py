979bf8d12ce1118f1c3c9bfd9099ce04e62af135
Mustafa-ismail-Eyi <eyi201201@gmail.com>
2020-12-13 15:52:12 +0300

    Update rbflayer.py

--- rbflayer.py
+++ rbflayer.py
@@ -14,17 +14,17 @@ class InitCentersRandom(Initializer):
     """
 
     def __init__(self, X):
-        self.X = X
+            self.X = X
 
-    def __call__(self, shape, dtype=None):
-	assert shape[1] == self.X.shape[1]
-  	idx = np.random.randint(self.X.shape[0], size=shape[0])
+        def __call__(self, shape, dtype=None):
+        assert shape[1] == self.X.shape[1]
+        idx = np.random.randint(self.X.shape[0], size=shape[0])
 
-	# type checking to access elements of data correctly
-  	if type(self.X) == np.ndarray:
-    		return self.X[idx, :]
-  	elif type(self.X) == pd.core.frame.DataFrame:
-    		return self.X.iloc[idx, :]
+        # type checking to access elements of data correctly
+        if type(self.X) == np.ndarray:
+                return self.X[idx, :]
+        elif type(self.X) == pd.core.frame.DataFrame:
+                return self.X.iloc[idx, :]
 
 
 class RBFLayer(Layer):
